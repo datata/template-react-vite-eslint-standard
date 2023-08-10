89ffcf53d6ec2553aa99a2583db44e0a9b8448c7
datata <dani@danitarazona.dev>
2023-08-10 17:25:56 +0200

    feat: Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off'
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <>
+      <h1>
+        Template ready with eslint Standard
+      </h1>
+    </>
+  )
+}
+
+export default App
