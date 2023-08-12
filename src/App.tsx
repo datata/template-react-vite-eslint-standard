7cc7750a845bafba2b5cd22f73955b8d5aee625e
datata <dani@danitarazona.dev>
2023-08-12 17:39:33 +0200

    feat: spinner added to loading users List

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './index.css'
 function App () {
   return (
     <>
-      <h1 className='flex justify-center mt-5 font-bold text-3xl'> Listado de usuarios RANDOM USER API</h1>
+      <h1 className='flex justify-center m-5 font-bold text-3xl'> Listado de usuarios RANDOM USER API</h1>
       <UsersList />
     </>
   )
